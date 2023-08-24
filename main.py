c88966ca8353afd9fde251cec6cb6ba8d89d99fb
Grigorii Tashu <grigoriytashu@gmail.com>
2023-08-24 17:34:04 +1000

    nejdi

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello")
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6475754729:AAFNddgHy0Y0BKnV1_gzrkkWFCjq1JONtq0'
+# TEXT: str = input()
+MAX_COUNTER: int = 100
+
+offset: int = -2
+counter: int = 0
+chat_id: int
+
+while counter < MAX_COUNTER:
+    print('attempt =', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+
+        for result in updates['result']:
+            print(result['message']['text'])
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={input()}')
+
+    time.sleep(1)
+    counter += 1
